ea1b82db889570ba5e9fc14a613fc2e34234eb61
Ben Bauer <benbauer314@gmail.com>
2019-03-29 16:16:48 -0700

    add loop to repeat tests, add ipc parts delta time
    ipc parts delta time prints the difference between the IPC time and the sum 2 syscalls and 2 cr3 refreshes

--- user/init.c
+++ user/init.c
@@ -27,55 +27,59 @@ main(void)
   
   dup(0);  // stdout
   dup(0);  // stderr
-  pid = fork();
-  if(pid == 0){
-    struct msg m;
-    recv(0,&m);
-    for(unsigned long long i = 0; i<ITERS-1ul; i++){
-      send_recv(0, &m);
+
+  for(int i = 0; i < 10; i++){
+    pid = fork();
+    if(pid == 0){
+      struct msg m;
+      recv(0,&m);
+      for(unsigned long long i = 0; i<ITERS-1ul; i++){
+        send_recv(0, &m);
+      }
+      send(0, &m);
+      exit();
+    }else{
+      struct msg m;
+      sleep(100);
+
+      unsigned long long reload_time = rdtsc();
+      for(unsigned long long i = 0; i<ITERS; i++){
+        cr3_test();
+      }
+      reload_time = rdtsc() - reload_time;
+      reload_time /= ITERS;
+      printf(1, "reload time - %d\n", reload_time);
+
+      unsigned long long ipc_time = rdtsc();
+      for(unsigned long long i = 0; i<ITERS; i++){
+        send_recv(0, &m);
+      }
+      ipc_time = rdtsc() - ipc_time;
+      ipc_time /= ITERS;
+      printf(1, "ipc time - %d\n",ipc_time);
+
+      unsigned long long null_call_time = rdtsc();
+      for(unsigned long long i = 0; i<ITERS; i++){
+        null_call();
+      }
+      null_call_time = rdtsc() - null_call_time;
+      null_call_time /= ITERS;
+      printf(1, "null_call time - %d\n",null_call_time);
+
+      unsigned long long cr3_kernel_time = rdtsc();
+      cr3_kernel(ITERS);
+      cr3_kernel_time = rdtsc() - cr3_kernel_time;
+      cr3_kernel_time /= ITERS;
+      printf(1, "cr3_kernel time - %d\n",cr3_kernel_time);
+
+      printf(1, "ipc/reload time delta - %d\n",ipc_time - reload_time);
+      printf(1, "ipc/sum of parts delta - %d\n", ipc_time - (2*(reload_time-null_call_time) + 2*null_call_time));
+      wait();
+      //exit();
     }
-    send(0, &m);
-    exit();
-  }else{
-    struct msg m;
-    sleep(100);
-
-    unsigned long long reload_time = rdtsc();
-    for(unsigned long long i = 0; i<ITERS; i++){
-      cr3_test();
-    }
-    reload_time = rdtsc() - reload_time;
-    reload_time /= ITERS;
-    printf(1, "reload time - %d\n", reload_time);
-
-    unsigned long long ipc_time = rdtsc();
-    for(unsigned long long i = 0; i<ITERS; i++){
-      send_recv(0, &m);
-    }
-    ipc_time = rdtsc() - ipc_time;
-    ipc_time /= ITERS;
-    printf(1, "ipc time - %d\n",ipc_time);
-
-    unsigned long long null_call_time = rdtsc();
-    for(unsigned long long i = 0; i<ITERS; i++){
-      null_call();
-    }
-    null_call_time = rdtsc() - null_call_time;
-    null_call_time /= ITERS;
-    printf(1, "null_call time - %d\n",null_call_time);
-
-    unsigned long long cr3_kernel_time = rdtsc();
-    cr3_kernel(ITERS);
-    cr3_kernel_time = rdtsc() - cr3_kernel_time;
-    cr3_kernel_time /= ITERS;
-    printf(1, "cr3_kernel time - %d\n",cr3_kernel_time);
-
-    printf(1, "ipc/reload time delta - %d\n",ipc_time - reload_time);
-    wait();
-    //exit();
-  }
   
-  printf(1, "uptime - %d\n", send(0, 90));
+    printf(1, "uptime - %d\n", send(0, 90));
+  }
   for(;;){
     printf(1, "init: starting sh\n");
     pid = fork();
